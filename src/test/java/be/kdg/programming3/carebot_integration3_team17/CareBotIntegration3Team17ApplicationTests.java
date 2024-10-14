63840c3c85286352b9b8c52a28575d97fc7a4f3f
Daria Jaroszuk <daria.jaroszuk@student.kdg.be>
2024-10-14 14:02:50 +0200

    creating a project

---
+++ src/test/java/be/kdg/programming3/carebot_integration3_team17/CareBotIntegration3Team17ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.programming3.carebot_integration3_team17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareBotIntegration3Team17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
