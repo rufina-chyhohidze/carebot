9a222cf4baf62669b14ad00592c08a5483562ee4
Petra Ádám <petra.adam@student.kdg.be>
2024-11-18 10:44:58 +0100

    Navigation between pages, fixes and designing

---
+++ src/main/java/be/kdg/programming3/controller/MainController.java
@@ -0,0 +1,33 @@
+package be.kdg.programming3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Maps to index.html
+    }
+
+    @GetMapping("/requestform")
+    public String requestForm() {
+        return "request-form"; // Maps to requestform.html
+    }
+
+    @GetMapping("/sendrobot")
+    public String sendRobot() {
+        return "send-robot"; // Maps to sendrobot.html
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Maps to warehouse.html
+    }
+
+    @GetMapping("/warehouse")
+    public String warehouse() {
+        return "warehouse";
+    }
+}
